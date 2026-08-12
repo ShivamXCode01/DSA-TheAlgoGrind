117009b872ae4c3ffb3477b410c9cc14a9cccd85
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-12 20:36:25 +0530

    Foundation

---
+++ Things_to_know_in_any_Langa/DATA-TYPES/sumof2numbers.cpp
@@ -0,0 +1,15 @@
+/*2. Sum of Two Numbers
+Take two integers as input, store them in variables, and print their sum.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b ;
+   
+    cout<<"Enter the value of a : "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:  "<<endl;
+    cin>>b;
+    int sum=a+b;
+    cout<<"The sum of a and b are: "<<sum <<endl;
+}
