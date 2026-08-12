222271a8ff3b95ed39e782a8510b5194a4e8e2ff
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-12 20:35:12 +0530

    Foundation

---
+++ Things_to_know_in_any_Langa/DATA-TYPES/personaldetails.cpp
@@ -0,0 +1,19 @@
+/*10. Store and Print Personal Details
+Declare variables for name (string), age (int), height (float), 
+and grade (char), assign values, and print them.*/
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+   string name;
+    int age;
+    float height;
+    cout<<"Enter your name: "<<endl;
+    cin>>name;
+    cout<<"Enter your age: " <<endl;
+    cin>>age;
+    cout<<"Enter your height in cm: "<<endl;
+    cin>>height;
+    cout<<"YOur height is :"<<height;
+    
+}
