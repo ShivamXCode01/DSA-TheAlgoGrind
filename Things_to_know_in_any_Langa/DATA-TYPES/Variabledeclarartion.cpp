f0cf0dfa21e0af747f50ce5065a42798e9528284
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-12 20:37:35 +0530

    Foundation

---
+++ Things_to_know_in_any_Langa/DATA-TYPES/Variabledeclarartion.cpp
@@ -0,0 +1,12 @@
+/*1. Declare and Print Variables
+Create variables of type int, float, char, and string (or equivalent in C/Python). 
+Assign values and print them.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    float f= 9.6;
+    char ch='s';
+    cout<< a <<endl << f <<endl <<ch <<endl;
+    return 0;
+}
