d31caf09baa7118be48e326d9c6909428dbfe22e
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-12 20:33:59 +0530

    Foundation

---
+++ Things_to_know_in_any_Langa/DATA-TYPES/6typeconversion.cpp
@@ -0,0 +1,13 @@
+/*7. Type Conversion
+Take an integer as input, convert it to float, and print both values.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter integer a:";
+    cin>>a;
+    float f= a;
+    cout<<a <<endl;
+    cout<<f;
+
+}
