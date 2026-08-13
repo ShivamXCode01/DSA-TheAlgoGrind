8066f6c3eeff3e8483ed7f03f75f2e24fb932a27
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-13 22:46:41 +0530

    FOUNDATION

---
+++ Things_to_know_in_any_Langa/LOOPS/1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<< "enter n ";
+    cin>> n ;
+    for(int i=0 ; i <= n ; i++){
+  
+        int p = i * i ;
+        cout<< p << endl ;
+
+        
+}
+    return 0;
+}
