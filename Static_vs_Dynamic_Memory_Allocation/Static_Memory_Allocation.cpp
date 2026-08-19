ebad915c72305207773176818ad40226187f4429
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-19 21:51:35 +0530

    Updated

---
+++ Static_vs_Dynamic_Memory_Allocation/Static_Memory_Allocation.cpp
@@ -0,0 +1,20 @@
+// Static Memory Allocation :- In this condition memory is allocated at the compile time. It is used where we know the size of memory 
+// before the compile time. Here, we can't define or resize the memory during runtime 
+
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    // 1-D Array 
+    // Creation of array using static memory allocation 
+
+    int arr[5] = {4,2,12,5,3};
+
+
+    // 2-D Array 
+    int brr[3][3] = {3,2,3,2,32,3,2,4,3};
+
+    //Here once we declare the size of array then after we can't resize or change the size of array.
+
+}
