2679d1836beaa1f7d3511169b2daba8d48b30dc9
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-22 22:09:43 +0530

    Updated

---
+++ Linked_List/Remove_Duplicate_in_sorted_LL.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+// Creating Node class
+
+class Node{
+    public:
+    int data ;
+    Node * next;
+
+    // Constructor 
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+  
+// insert tail
+
+void insertAtTail(Node*&head,int d){
+    Node * newNode = new Node(d);
+    // Empty LL handled
+    if (head == NULL){
+       head = newNode ; 
+    }
+
+    // Non Empty LL
+    Node*temp = head;
+    while (temp->next != NULL){
+        temp = temp -> next;
+    };
+    temp -> next = newNode;
+    cout << d <<" Inserted Successfully! :-  ";
+
+    
+}
+
+// traversal of LL
+
+void print(Node *&head){
+    Node * temp = head;
+    
+    while (temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+// Remove Duplicates in sorted LL
+
+Node * removeDuplicate(Node * head){
+    // empty LL
+    if(head == NULL){
+        return NULL;
+    }
+    Node * curr = head;
+
+    while(curr != NULL){
+        if ((curr -> next != NULL) && (curr -> data == curr -> next -> data)){
+            Node *next_next = curr -> next -> next;
+            Node *nodeToDelete = curr -> next;
+            delete(nodeToDelete);
+            curr -> next = next_next;
+        }
+        else{
+            curr = curr -> next;
+        }
+    }
+    return head;
+
+}
+
+
+
+
+int main(){
+    Node * node1 = new Node(1);
+    Node * head = node1;
+
+    cout << "Initail LL is 5 \n";
+
+    insertAtTail(head,3);
+    print(head);
+
+    insertAtTail(head,4);
+    print(head);
+
+
+    insertAtTail(head,4);
+    print(head);
+
+    insertAtTail(head,5);
+    print(head);
+
+    insertAtTail(head,6);
+    print(head);
+
+    insertAtTail(head,6);
+    print(head);
+
+    Node * result = removeDuplicate(head);
+    cout << "Printing LL after removing Duplicates :- ";
+    print(result);
+    
+
+    
+
+}
