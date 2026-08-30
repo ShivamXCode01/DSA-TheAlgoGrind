a78119cafc6e8bee1381db314ad167be2ada7212
Shivam_001 <shivam.soni.aiml@gmail.com>
2026-08-30 22:11:03 +0530

    Updated

--- Stack/Stack_implementation_STL.cpp
+++ Stack/Stack_implementation_STL.cpp
@@ -46,6 +46,7 @@ int main (){
     num.push(3);
     num.push(9);
     num.push(4);
+    num.push(0);
 
     cout << "Top/Peek of the stack : " <<num.top() << endl;
     cout << "Size of stack after insertion : " << num.size() << endl;
